44b6d3fc5c37c295a769a372a3a09fef1449e3bd
tohirul <tohirul.islam164@gmail.com>
2025-02-08 16:06:02 +0600

    [Message] sequelize model

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testMatch: ['**/tests/**/*.test.ts', '**/?(*.)+(spec|test).ts'],
+};
+
+export default config;
