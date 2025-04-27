9f2f46fa644cb5cffb8e2282721e2ba7e295457b
tohirul <tohirul.islam164@gmail.com>
2025-04-27 13:08:20 +0600

    [Chore] Repository for Airport Created

---
+++ src/core/repositories/container/repository_airport.ts
@@ -0,0 +1,19 @@
+import { injectable } from 'tsyringe';
+
+import PrismaService from '@/database';
+
+import Repository from '../repository';
+
+import type { Airport, PrismaClient } from "@generated/@prisma/client";
+
+
+const Prisma = PrismaService.client;
+
+@injectable()
+class AirportRepository extends Repository<Airport, PrismaClient['airport']> {
+  constructor() {
+    super(Prisma.airport);
+  }
+}
+
+export default AirportRepository;
